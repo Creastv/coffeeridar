056a3b4178417fb462d529b41224dadb72f10cfc
Creastv <piotr@roial.pl>
2022-08-24 15:54:25 +0200

    ch

--- templates-parts/extras/stickers.php
+++ templates-parts/extras/stickers.php
@@ -5,7 +5,7 @@ $st_link = get_theme_mod('st_link');
     <?php if( $st_link ){ ?>
     <div class="stickier st-1">
         <div class="wraper">
-            <a href="<?php echo $st_link; ?>">
+            <a href="<?php echo $st_link; ?>" target="_blank">
             <img src="<?php echo get_template_directory_uri() ?>/src/img/icon-sti.png" alt="">
             <span>zobacz nasz serwis na mapie</span>
             </a>
@@ -15,7 +15,7 @@ $st_link = get_theme_mod('st_link');
     <div class="stickier st-2">
         
         <div class="wraper modal-opener js">
-            <a href="#" target="_blank" class="">
+            <a href="#"  class="">
             <img src="<?php echo get_template_directory_uri() ?>/src/img/icon-sti.png" alt="">
             <span>Umów się na serwis</span>
             </a>
