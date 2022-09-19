3ce67d226d9bebeb3f75404043729e7070bc3875
Creastv <piotr@roial.pl>
2022-09-19 16:36:54 +0200

    fixing

--- templates-parts/header/navbar.php
+++ templates-parts/header/navbar.php
@@ -1,4 +1,7 @@
 <div class="navbar js">
+    <div class="active-logo">
+    <?php get_template_part('templates-parts/header/branding'); ?>
+    </div>
     <?php get_template_part('templates-parts/header/social-media'); ?>
     <?php get_template_part('templates-parts/header/contact'); ?>
     <div class="toggler-nav js">
