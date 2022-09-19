9790912e9c927398c7144b19790d8d550ce6b992
Creastv <piotr@roial.pl>
2022-09-19 16:52:40 +0200

    fixing

--- src/js/main.js
+++ src/js/main.js
@@ -8,10 +8,12 @@
     if (navFlag == false) {
       headerNav.classList.add("active");
       togglerNav.classList.add("active");
+      document.querySelector("#header").classList.add("active");
       navFlag = true;
     } else {
       headerNav.classList.remove("active");
       togglerNav.classList.remove("active");
+      document.querySelector("#header").classList.remove("active");
       navFlag = false;
     }
   });
